6973205bb1c7e5e70f0fa059bc7540d3e787ad85
alucenx <127796328+alucenx@users.noreply.github.com>
2024-01-19 19:23:44 +0200

    Add files via upload

---
+++ FetchFileShareKey.ps1
@@ -0,0 +1,9 @@
+﻿$storageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName
+if ($storageAccountKeys.Count -eq 0) {
+    throw 'No storage account keys found.'
+}
+$storageAccountKey = $storageAccountKeys[0].Value
+$cmdkeyCommand = "cmdkey.exe /add:$fileShareName /user:$storageAccountName /pass:$storageAccountKey"
+Invoke-Expression -Command $cmdkeyCommand
+[System.Environment]::SetEnvironmentVariable("StorageAccountKey", $storageAccountKey, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("TemplateName", "${imageTemplateName}", [System.EnvironmentVariableTarget]::Machine)
